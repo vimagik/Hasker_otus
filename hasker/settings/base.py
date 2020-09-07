ee01514b40b1e25ce9e1571eba43da8ba6426665
Rzhavin Georgii <rzhavin@my.com>
2020-09-07 21:14:01 +0300

    Reduced errors Static path

--- hasker/settings/base.py
+++ hasker/settings/base.py
@@ -100,6 +100,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    './static/',
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '../../media/')
 LOGIN_URL = '/auth/login/'
